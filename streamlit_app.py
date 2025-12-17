257126c367cc2c872f0e47bc8ed4e52c0a417ccd
Roozbeh Daneshvar <roozbeh.daneshvar@gmail.com>
2025-12-17 10:55:36 -0500

    Update streamlit_app.py
    Add new column and test

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,10 @@ st.title("Customize Your Smoothie :beach_umbrella:")
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     label='Choose up to six',
