5c0ea7a07848a363731d384c55f1743a27ae567d
Roozbeh Daneshvar <roozbeh.daneshvar@gmail.com>
2025-12-17 10:56:22 -0500

    Update streamlit_app.py
    import pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import streamlit as st
+import pandas as pd
 
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The bame on smoothie is", name_on_order)
